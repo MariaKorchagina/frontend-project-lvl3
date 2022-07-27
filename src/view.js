8cb65ce436b725235d695704ac5fc04e7f11861e
Maria Korchagina <kobysheva_maria@mail.ru>
2022-07-28 00:05:44 +0300

    corrected read case

--- src/view.js
+++ src/view.js
@@ -172,11 +172,11 @@ const render = (state) => (path, value) => {
           switch (post.status) {
             
             case 'read':
-              // postItem.classList.remove('fw-bold');
+               postItem.classList.remove('fw-bold');
               postItem.classList.add('fw-normal');
               break;
             case 'unread':
-              postItem.classList.add('fw-bold');
+              postItem.classList.add('fw-normal');
               break;
             default:
               throw new Error(`Unknown status: ${post.status}`);
