61a77077bf495132172e0056227d6fff022ddf13
Maria Korchagina <kobysheva_maria@mail.ru>
2022-07-30 23:46:50 +0300

    check

--- src/locales/en.js
+++ src/locales/en.js
@@ -3,8 +3,8 @@ export default {
     errors: {
       notValidUrl: 'Link has not a valid URL. Please try again.',
       sameUrl: 'RSS request already exists',
-      'notValidLink': 'RSS does not contain a valid link. Please try again.',
-      'networkError': 'Network Error. Please try again.',
+      notValidLink: 'RSS does not contain a valid link. Please try again.',
+      networkError: 'Network Error. Please try again.',
       'error 404': 'Error 404: Page not found. Please try again.',
       'error 405': 'Error 405: Method Not Allowed. Please try again.',
       'error 406': 'Error 406: Website does not support the client\'s request with a particular protocol. Please try again.',
